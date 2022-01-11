c975557f18418330d428fc58ecee0ed1f2ad98c6
Ryan Brockley <rybrockers@yahoo.com>
2022-01-11 19:57:41 +0000

    init

---
+++ src/components/Navbar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="nav">
+       <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'navbar'
+
+}
+</script>
+
+<style>
+
+</style>
