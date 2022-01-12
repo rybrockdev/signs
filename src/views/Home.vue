484b27443514f46a471a2cd5c1cbfff5013236b5
Ryan Brockley <rybrockers@yahoo.com>
2022-01-12 00:04:53 +0000

    fleshed out homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="home">
     <div class="container">
-    <h1 class="text-center mt-5 mb-5">This is the homepage</h1>
-       <div class="row">
+      <h1 class="text-center mt-5 mb-5">This is the homepage</h1>
+      <div class="row">
         <div class="about-image col-sm-6">
           <img
             src="https://via.placeholder.com/600x500"
@@ -10,7 +10,16 @@
             class="w-100 mb-3"
           />
         </div>
-        <div class="about-text col-sm-6 d-flex flex-column justify-content-center align-items-center">
+        <div
+          class="
+            about-text
+            col-sm-6
+            d-flex
+            flex-column
+            justify-content-center
+            align-items-center
+          "
+        >
           <p class="fs-3 fw-bold">Only the best in industry standards</p>
           <p class="text-center">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse,
@@ -18,13 +27,13 @@
             error sit non doloremque enim modi dolor possimus ut, recusandae
             quos sed expedita!
           </p>
-            <p class="text-center">
+          <p class="text-center">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse,
             accusamus eveniet. Eos illo nemo, reprehenderit voluptatum earum
             error sit non doloremque enim modi dolor possimus ut, recusandae
             quos sed expedita!
           </p>
-            <p class="text-center">
+          <p class="text-center">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse,
             accusamus eveniet. Eos illo nemo, reprehenderit voluptatum earum
             error sit non doloremque enim modi dolor possimus ut, recusandae
@@ -32,8 +41,117 @@
           </p>
         </div>
       </div>
-
+      <div class="recent container mb-5">
+        <p class="text-center fs-3 fw-bold mt-5">Our most recent Signs</p>
+        <div class="recent-signs row">
+          <div class="col-sm-3">
+            <img
+              src="https://via.placeholder.com/600x500"
+              alt=""
+              class="w-100 mb-3"
+            />
+          </div>
+            <div class="col-sm-3">
+            <img
+              src="https://via.placeholder.com/600x500"
+              alt=""
+              class="w-100 mb-3"
+            />
+          </div>
+            <div class="col-sm-3">
+            <img
+              src="https://via.placeholder.com/600x500"
+              alt=""
+              class="w-100 mb-3"
+            />
+          </div>
+            <div class="col-sm-3">
+            <img
+              src="https://via.placeholder.com/600x500"
+              alt=""
+              class="w-100 mb-3"
+            />
+          </div>
+        </div>
+      </div>
     </div>
+    <div class="container">
+  <div class="row recent-workwear">
+       <h1 class="text-center mt-5 mb-5">Check out our Workwear</h1>
+        <div
+          class="
+            about-text
+            col-sm-6
+            d-flex
+            flex-column
+            justify-content-center
+            align-items-center
+          "
+        >
+          <p class="fs-3 fw-bold">Only the best in industry standards</p>
+          <p class="text-center">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse,
+            accusamus eveniet. Eos illo nemo, reprehenderit voluptatum earum
+            error sit non doloremque enim modi dolor possimus ut, recusandae
+            quos sed expedita!
+          </p>
+          <p class="text-center">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse,
+            accusamus eveniet. Eos illo nemo, reprehenderit voluptatum earum
+            error sit non doloremque enim modi dolor possimus ut, recusandae
+            quos sed expedita!
+          </p>
+          <p class="text-center">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse,
+            accusamus eveniet. Eos illo nemo, reprehenderit voluptatum earum
+            error sit non doloremque enim modi dolor possimus ut, recusandae
+            quos sed expedita!
+          </p>
+        </div>
+         <div class="about-image col-sm-6">
+          <img
+            src="https://via.placeholder.com/600x500"
+            alt=""
+            class="w-100 mb-3"
+          />
+        </div>
+        
+      </div>
+    </div>
+   
+            <div class="recent container mb-5">
+        <p class="text-center fs-3 fw-bold mt-5">Our most recent Workwear</p>
+        <div class="recent-signs row">
+          <div class="col-sm-3">
+            <img
+              src="https://via.placeholder.com/600x500"
+              alt=""
+              class="w-100 mb-3"
+            />
+          </div>
+            <div class="col-sm-3">
+            <img
+              src="https://via.placeholder.com/600x500"
+              alt=""
+              class="w-100 mb-3"
+            />
+          </div>
+            <div class="col-sm-3">
+            <img
+              src="https://via.placeholder.com/600x500"
+              alt=""
+              class="w-100 mb-3"
+            />
+          </div>
+            <div class="col-sm-3">
+            <img
+              src="https://via.placeholder.com/600x500"
+              alt=""
+              class="w-100 mb-3"
+            />
+          </div>
+        </div>
+      </div>
   </div>
 </template>
 
@@ -41,8 +159,6 @@
 // @ is an alias to /src
 
 export default {
-  components: {
-   
-  }
-}
+  components: {},
+};
 </script>
